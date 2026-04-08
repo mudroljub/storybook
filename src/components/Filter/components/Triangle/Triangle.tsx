91a4f92d248f25a8453438466c465d675acdd749
Damjan Pavlica <mudroljub@gmail.com>
2026-04-08 16:16:39 +0200

    m

--- src/components/Filter/components/Triangle/Triangle.tsx
+++ src/components/Filter/components/Triangle/Triangle.tsx
@@ -5,15 +5,17 @@ interface TriangleProps {
   isDown?: boolean
 }
 
-const Triangle: React.FC<TriangleProps> = ({ 
-  isDown = false, 
-}) => {
-  const upPath = "M11 0.5 L21.5 11.5 H0.5 Z"
-  const downPath = "M11 11.5 L0.5 0.5 H21.5 Z"
+const Triangle = ({ isDown = false }: TriangleProps) => {
+  /**
+   * Paths are relative (0-100) to fill SCSS dimensions; 
+   * 0.5 offsets prevent stroke clipping.
+   */
+  const upPath = "M50 0.5 L99.5 99.5 H0.5 Z"
+  const downPath = "M50 99.5 L0.5 0.5 H99.5 Z"
 
   return (
     <svg 
-      viewBox="0 0 22 12" 
+      viewBox="0 0 100 100" 
       className={styles.triangle}
       preserveAspectRatio="none"
     >
