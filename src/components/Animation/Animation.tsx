092e3fafa3099e838bb379e923f95589773416c9
Damjan Pavlica <mudroljub@gmail.com>
2026-05-05 16:33:45 +0200

    animation update

--- src/components/Animation/Animation.tsx
+++ src/components/Animation/Animation.tsx
@@ -4,7 +4,12 @@ const Animation = ({ count = 10 }) => {
   return (
     <div className={styles.parent}>
       {Array.from({ length: count }).map((_, i) => (
-        <span key={i} className={styles.span} />
+        <span key={i}
+          className={styles.span}
+          style={{
+            animationDelay: `${Math.random() * 0.8}s`,
+          }}
+        />
       ))}
     </div>
   )
